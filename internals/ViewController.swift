d2f90dbb714164e422034f222404e5584b5ca5af
ke.liang <lk@tary-mbp.lan>
2017-08-26 11:09:09 +0800

    Initial Commit

---
+++ internals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  internals
+//
+//  Created by ke.liang on 2017/8/26.
+//  Copyright © 2017年 lk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
