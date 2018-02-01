e7302e172f40dfa05ce52aa14b0c5ff318965c9d
liangke <lokyse@163.com>
2018-02-01 23:21:17 +0800

    个人主页布局
    Signed-off-by: liangke <lokyse@163.com>

--- internals/PersonViewController.swift
+++ internals/PersonViewController.swift
@@ -8,47 +8,111 @@
 
 import UIKit
 
+
+
+fileprivate let SectionN = 2
+fileprivate let tableHeaderH:CGFloat = ScreenH / 3 
+
 class PersonViewController: UIViewController {
 
+    private lazy var table:UITableView = { [unowned self] in
+        let table = UITableView.init(frame: self.view.bounds)
+        table.isScrollEnabled = false
+        table.tableFooterView = UIView.init()
+        table.delegate = self
+        table.dataSource = self
+        
+        return table
+    }()
     
-    lazy var  logoutButton:UIButton = {
-       let button = UIButton.init(type: UIButtonType.custom)
-        button.setTitle("退出登录", for: .normal)
-        button.backgroundColor = UIColor.blue
-        button.addTarget(self, action: #selector(logout), for: .touchUpInside)
-        return button
+    
+    private lazy var tableHeader: personTableHeader = {
+        let v = personTableHeader.init(frame:CGRect.init(x: 0, y: 0, width: ScreenW, height: tableHeaderH))
+        return v
         
     }()
-        
+    
+    private var mode:[(image:UIImage, title:String)] = [(#imageLiteral(resourceName: "namecard"),"我的名片"),(#imageLiteral(resourceName: "settings"),"账号设置"),(#imageLiteral(resourceName: "collection"),"我的收藏"),(#imageLiteral(resourceName: "feedback"),"意见反馈")]
+    
     
     
     override func viewDidLoad() {
-        self.view.addSubview(logoutButton)
-        
         super.viewDidLoad()
-        
+        self.view.backgroundColor = UIColor.clear
         // Do any additional setup after loading the view.
     }
 
     
     override func viewWillLayoutSubviews() {
-        _ = self.logoutButton.sd_layout().topSpaceToView(self.view,60)?.centerXEqualToView(self.view)?.widthIs(100)?.heightIs(20)
+        self.view.addSubview(table)
+        self.table.tableHeaderView = tableHeader
+        
         
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+}
+
+
+extension PersonViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SectionN
     }
     
-
-    @objc  func logout(){
-        // 清理数据
-        
-        let loginView = self.storyboard?.instantiateViewController(withIdentifier: "login")  as! LogViewController
-        
-        self.present(loginView, animated: true, completion: nil)
-        
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  section == 0 ? 1 : self.mode.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = UITableViewCell.init()
+            cell.textLabel?.text = "我的简历"
+            return cell
+            
+        case 1:
+            let cell = UITableViewCell.init()
+            cell.textLabel?.text = self.mode[indexPath.row].title
+            
+            return cell
+        default:
+            return UITableViewCell.init()
+            
+        }
         
     }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let view = UIView.init(frame: CGRect.zero)
+        view.backgroundColor = UIColor.lightGray
+        return view
+        
+    }
+    
+    
+    
+}
 
+
+private class personTableHeader:UIView{
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        self.backgroundColor = UIColor.orange
+        
+    }
 }
